1f48051bdc21686fc273daca7df4aad9cd18625f
gulufei <315077389@qq.com>
2019-08-19 17:29:23 +0800

    简单login组件

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,48 @@
-import {Component} from 'react'
+import React, { Component } from 'react'
+import LoginUi from './loginUi';
+import axios from 'axios'
 
-function f() {
+export default class Login extends Component {
 
+    constructor(props){
+        super(props)
+        this.state={
+            username:"",
+            password:""
+        }
+        this.usernameChange = this.usernameChange.bind(this)
+        this.passwordChange = this.passwordChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    usernameChange(event){
+        this.setState({username:event.target.value})
+    }
+
+    passwordChange(event){
+        this.setState({password:event.target.value})
+    }
+
+    handleSubmit(event){
+        axios.post('http://localhost:8080/Login',
+            {
+                username:this.state.username,
+                password:this.state.password
+            }
+        )
+        .then(function(response){
+            alert(response.data)
+        })
+        .catch(function(error){
+            alert(error)
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <LoginUi usernameChange={this.usernameChange} passwordChange={this.passwordChange} handleSubmit={this.handleSubmit}/>
+            </div>
+        )
+    }
 }
